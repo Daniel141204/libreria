9eba769821dba7aa92cfea018c359bf6f366d058
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2023-09-20 18:00:27 -0500

    Creación conexión
    Poniendo librerias base (comentar las k no se van a utilizar)

---
+++ conexion.py
@@ -0,0 +1,9 @@
+#***LIBRERÍAS***
+
+from sodapy import Socrata #Para obtener datasets de url
+import pandas as pd #Manejo de datasets
+import numpy as np #Manejo de matrices, vectores y otras operaciones matematicas
+import matplotlib.pyplot as plt #graficación de datos
+import seaborn as sns #mapas de calor
+
+#***FUNCIONES***
