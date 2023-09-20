d658796c689909c7cec7a7225318ff4adaae04db
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2023-09-20 18:46:17 -0500

    Conexión (utilizando polimorfismo)
    Hola bb

--- conexion.py
+++ conexion.py
@@ -9,7 +9,57 @@ import seaborn as sns #mapas de calor
 #***FUNCIONES***
 
 #función que se encarga de recoger el nombre del cvs o la url
+"""
 def recogerDataset(nombre): 
+    
+    dataset=abrirCVS(nombre)
+    if dataset==False:
+       dataset=abrirURL(nombre)
+       if dataset==False:
+           print("No se encontró ningún archivo con el nombre brindado o con dicha URL")
+       else:    
+           print("The dataset has been open successfully.")
+               
+
     try:
         dataset=abrirCVS(nombre)
+    except Exception as err:
+        dataset=abrirURL(nombre)
     return "yo"
+    """
+
+#función que se encarga de abrir cvs
+def abrirCVS(nombre):
+    try:
+        dataset=pd.read_csv(nombre)
+    except FileNotFoundError:
+        dataset=False
+    return dataset
+
+
+#función que se encarga de abrir una url
+def abrirCVS(url, codigo):
+    try:
+        client=Socrata(url, None)
+        results=client.get(codigo)
+        dataset=pd.DataFrame.from_records(results)
+    except FileNotFoundError:
+        dataset=False
+    return dataset
+
+
+#función que se encarga de abrir una url con limite
+def abrirCVS(url, codigo, limite):
+    try:
+        client=Socrata(url, None)
+        results=client.get(codigo, limit=limite)
+        dataset=pd.DataFrame.from_records(results)
+    except ConnectionError:
+        dataset=False
+    return dataset
+
+print("hola soy una prueba\n")
+d1=(abrirCVS('www.datos.gov.co','gt2j-8ykr', 50))
+d1
+print("ya abrí jajaj")
+
