dd17aa959f793b476ee6588a2d71dc467ed4f28a
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2023-09-20 18:11:16 -0500

    Avance conexión
    hola amorcito

--- conexion.py
+++ conexion.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt #graficación de datos
 import seaborn as sns #mapas de calor
 
 #***FUNCIONES***
+
+#función que se encarga de recoger el nombre del cvs o la url
+def recogerDataset(nombre): 
+    try:
+        dataset=abrirCVS(nombre)
+    return "yo"
