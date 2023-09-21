3f9c9178a432f4a04e589bad5c78344c9fa1ddce
Daniel141204 <67084296+Daniel141204@users.noreply.github.com>
2023-09-20 19:31:19 -0500

    eliminar librerias de conexion
    pues eso

--- conexion.py
+++ conexion.py
@@ -2,9 +2,6 @@
 
 from sodapy import Socrata #Para obtener datasets de url
 import pandas as pd #Manejo de datasets
-import numpy as np #Manejo de matrices, vectores y otras operaciones matematicas
-import matplotlib.pyplot as plt #graficación de datos
-import seaborn as sns #mapas de calor
 
 #***FUNCIONES***
 
